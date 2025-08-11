89ad19dabe1864db3b096939b2a72dafffc58af2
Dhamilare <samuelholuwatosin@gmail.com>
2025-08-11 06:34:57 +0100

    changes

--- lmsApp/templatetags/custom_filters.py
+++ lmsApp/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -48,3 +49,26 @@ def replace(value, arg):
         return value # Return original value if splitting fails (e.g., no comma)
 
 
+@register.filter(name='embed_document')
+def embed_document(url):
+    """
+    Generates a URL for embedding PDFs and PPTs using Google Docs Viewer.
+    Assumes the URL is a direct link to the file.
+    
+    Usage:
+    <iframe src="{{ file.url|embed_document }}" style="..."></iframe>
+    """
+    if not url:
+        return ""
+    
+    # Check the file extension to see if it's a type that Google Docs Viewer can handle
+    # and if it's not already a youtube video.
+    file_type = url.split('.')[-1].lower()
+    
+    if file_type in ['pdf', 'ppt', 'pptx', 'doc', 'docx', 'xls', 'xlsx']:
+        # Google Docs Viewer URL format
+        embed_url = f"https://docs.google.com/gview?url={url}&embedded=true"
+        return mark_safe(embed_url)
+    
+    # If it's not a known document type, return the original URL
+    return mark_safe(url)
